2e99ff5378cea10933a301498165e1da301c0701
Ekojode Oma-Victor <ekojodeoma@gmail.com>
2025-10-22 16:15:21 +0100

    Added Application Accets

--- paxform_medical/lib/core/constants/app_assets.dart
+++ paxform_medical/lib/core/constants/app_assets.dart
@@ -1,1 +1,29 @@
-
+class AppAssets {
+  static const String checkShieldIcon = "assets/icons/bx-check-shield_icon.svg";
+  static const String calendarIcon = "assets/icons/calendar_icon.svg";
+  static const String calendarTickIcon = "assets/icons/calendar_tick_icon.svg";
+  static const String clockIcon = "assets/icons/clock_icon.svg";
+  static const String dermisIcon = "assets/icons/dermis_icon.svg";
+  static const String doctorAppointmentIcon =
+      "assets/icons/doctor_appointment_icon.svg";
+  static const String earIcon = "assets/icons/ear_icon.svg";
+  static const String heartBeat = "assets/icons/heart_beat.svg";
+  static const String heartIcon = "assets/icons/heart_icon.svg";
+  static const String hospitalAppointmentIcon =
+      "assets/icons/hospital_appointment_icon.svg";
+  static const String jointIcon = "assets/icons/joint_icon.svg";
+  static const String locationIcon = "assets/icons/location_icon.svg";
+  static const String lungsIcon = "assets/icons/lungs_icon.svg";
+  static const String mapIcon = "assets/icons/map_icon.svg";
+  static const String searchIcon = "assets/icons/search_icon.svg";
+  static const String sendIcon = "assets/icons/search_icon.svg";
+  static const String settingsIcon = "assets/icons/settings_icon.svg";
+  static const String starIcon = "assets/icons/star_icon.svg";
+  static const String stethoscopeIcon = "assets/icons/stethoscope_icon.svg";
+  static const String stomachIcon = "assets/icons/stomach_icon.svg";
+  static const String teethIcon = "assets/icons/teeth_icon.svg";
+  static const String drClark = "assets/images/dr_clark.png";
+  static const String drJason = "assets/images/dr_jason.png";
+  static const String drWhite = "assets/images/dr_white.png";
+  static const String drWilson = "assets/images/dr_wilson.png";
+}
