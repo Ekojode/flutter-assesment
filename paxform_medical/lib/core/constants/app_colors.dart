208dc780193b2860d7a66d82028817c59573c7cb
Ekojode Oma-Victor <ekojodeoma@gmail.com>
2025-10-22 13:40:22 +0100

    feat: added app color palette  and cleaned main.dart

---
+++ paxform_medical/lib/core/constants/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const primary = Color(0XFF0A61B4);
+  static const background = Color(0XFFFFFFFF);
+  static const primaryAccent = Color(0XFF0099FF);
+  static const backgroundAccent = Color(0XFFF6F7FB);
+  static const primaryText = Color(0XFF000000);
+  static const secondaryText = Color(0XFF333333);
+  static const altText = Color(0XFFFFFFFF);
+  static const cardBgPurple = Color(0XFFF9F5FF);
+  static const cardBgGreen = Color(0XFFEDFCF2);
+  static const success = Color(0XFF20976C);
+  static const ash = Color(0XFFD5D5D5);
+  static const like = Color(0XFFFF525E);
+}
